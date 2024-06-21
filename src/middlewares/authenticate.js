7a278649ad68fc725b958bebf2d995910a31f065
Cool-developer-975 <ameyakawade0@gmail.com>
2024-06-21 22:32:36 +0530

    made some minor changes

--- src/middlewares/authenticate.js
+++ src/middlewares/authenticate.js
@@ -4,13 +4,14 @@ export const rootAuthenticate = async (req, res, next) => {
     const token = req?.cookies?.token;
 
     if (!token) return next();
-
-    const user = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
-
-    if (!user) {
+    let user = null;
+    try {
+        user = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
+    } catch (error) {
         res.clearCookie('token');
         return next();
     }
+    
     req.user = user;
 
     res.redirect('/user/dashboard');
@@ -18,11 +19,14 @@ export const rootAuthenticate = async (req, res, next) => {
 
 export const authenticate = async (req, res, next) => {
     const token = req?.cookies?.token;
-
+    let user = null;
     if (!token) return res.redirect('/');
-
-    const user = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
-
+    try {
+        user = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
+    } catch (error) {
+        return res.clearCookie('token').redirect('/');
+    }
+    
     if (!user) return res.clearCookie('token').redirect('/');
 
     req.user = user;
